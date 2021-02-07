01fbf5716d3d868be95f47c861bce4a298bf68c2
yingcheng <yingchengpa@qq.com>
2021-02-07 14:28:21 +0800

    update to onvif2

--- onvif2/__init__.py
+++ onvif2/__init__.py
@@ -1,7 +1,7 @@
-from onvif.client import ONVIFService, ONVIFCamera, SERVICES
-from onvif.exceptions import ONVIFError, ERR_ONVIF_UNKNOWN, \
+from onvif2.client import ONVIFService, ONVIFCamera, SERVICES
+from onvif2.exceptions import ONVIFError, ERR_ONVIF_UNKNOWN, \
         ERR_ONVIF_PROTOCOL, ERR_ONVIF_WSDL, ERR_ONVIF_BUILD
-#from onvif import cli
+#from onvif2 import cli
 
 __all__ = ( 'ONVIFService', 'ONVIFCamera', 'ONVIFError',
             'ERR_ONVIF_UNKNOWN', 'ERR_ONVIF_PROTOCOL',
