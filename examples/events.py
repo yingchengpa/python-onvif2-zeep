81ff6e62ee2b91d049cb3b246d1cd1d86d72a2f9
yingcheng <yingchengpa@qq.com>
2021-06-09 16:42:52 +0800

    Update events.py
    support the event , hik、dahua ipc test pass

--- examples/events.py
+++ examples/events.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
-from onvif import ONVIFCamera
-__author__ = 'vahid'
-
+from onvif2 import ONVIFCamera
 
 if __name__ == '__main__':
     mycam = ONVIFCamera('192.168.1.10', 8899, 'admin', 'admin') #, no_cache=True)
-    event_service = mycam.create_events_service()
-    print(event_service.GetEventProperties())
-    
+    events_service = mycam.create_events_service()
+    print(events_service.GetEventProperties())
+
     pullpoint = mycam.create_pullpoint_service()
-    req = pullpoint.create_type('PullMessages')
-    req.MessageLimit=100
-    print(pullpoint.PullMessages({"Timeout":timedelta(seconds=2),"MessageLimit":10}))
+    
+    while True:
+        try:
+            pullmess = pullpoint.PullMessages({"Timeout": datetime.timedelta(seconds=5), "MessageLimit": 10})
+            print(pullmess.CurrentTime)
+            print(pullmess.TerminationTime)
+            for msg in pullmess.NotificationMessage:
+                print(msg)
+        except Exception as e:
+            print(e)
+        finally:
+            pass
